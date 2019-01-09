780c61222d170aa1d3577b1774867cca87e87e2c
Joshua Turner <joshflyer5tp@gmail.com>
2019-01-09 18:04:30 -0500

    Added class for the Logitech F310 gamepad button mapping.

---
+++ src/main/java/frc/robot/gamepads/F310.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.gamepads;
+
+/**
+ * Add your docs here.
+ */
+public final class F310 {
+    // Right side (letter) buttons.
+    public static final int A_BTN = 0;
+    public static final int B_BTN = 1;
+    public static final int X_BTN = 2;
+    public static final int Y_BTN = 3;
+
+    // Bumpers
+    public static final int L_BUMPER = 4;
+    public static final int R_BUMPER = 5;
+
+    // Back & Start
+    public static final int BACK = 6;
+    public static final int START = 7;
+
+    // Stick buttons
+    public static final int LS_BTN = 8;
+    public static final int RS_BTN = 9;
+}
