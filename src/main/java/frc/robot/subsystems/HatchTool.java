5fdf6b6d9976f50bf99490f8404384508cea061e
Joshua Turner <joshflyer5tp@gmail.com>
2019-01-10 19:00:23 -0500

    coment to show where components are init'd

--- src/main/java/frc/robot/subsystems/HatchTool.java
+++ src/main/java/frc/robot/subsystems/HatchTool.java
@@ -20,6 +20,8 @@ import frc.robot.RobotMap;
 public class HatchTool extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+  // ** Components **
   private final DoubleSolenoid claw_solenoid;
 
 
