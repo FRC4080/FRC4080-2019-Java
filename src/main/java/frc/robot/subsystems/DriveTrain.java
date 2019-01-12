c44cdbd9ca18f5a231efac9b641e2f9c6629c630
Joshua Turner <joshflyer5tp@gmail.com>
2019-01-12 16:51:04 -0500

    added base drive train class

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Robot drive train. Will be 4 REV NEOs (possibly + 2 MiniCIMs).
+ * All controllers on CAN bus.
+ */
+public class DriveTrain extends Subsystem {
+  // ** Components **
+  private final CANSparkMax leftMaster;
+  private final CANSparkMax leftFollower;
+  private final CANSparkMax rightMaster;
+  private final CANSparkMax rightFollower;
+
+  @Override
+  public void initDefaultCommand() {
+  }
+}
