7d9837fb420ab2ce366d2bac081e57fcc26b547d
Joshua Turner <joshflyer5tp@gmail.com>
2019-02-12 09:40:32 -0500

    button index starts at 1!?

--- src/main/java/frc/robot/gamepads/F310.java
+++ src/main/java/frc/robot/gamepads/F310.java
@@ -15,22 +15,22 @@ package frc.robot.gamepads;
 public final class F310 {
     // *** Buttons ***
     // Right side (letter) buttons.
-    public static final int A_BTN = 0;
-    public static final int B_BTN = 1;
-    public static final int X_BTN = 2;
-    public static final int Y_BTN = 3;
+    public static final int A_BTN = 1;
+    public static final int B_BTN = 2;
+    public static final int X_BTN = 3;
+    public static final int Y_BTN = 4;
 
     // Bumpers
-    public static final int L_BUMPER = 4;
-    public static final int R_BUMPER = 5;
+    public static final int L_BUMPER = 5;
+    public static final int R_BUMPER = 6;
 
     // Back & Start
-    public static final int BACK = 6;
-    public static final int START = 7;
+    public static final int BACK = 7;
+    public static final int START = 8;
 
     // Stick buttons
-    public static final int LS_BTN = 8;
-    public static final int RS_BTN = 9;
+    public static final int LS_BTN = 9;
+    public static final int RS_BTN = 10;
 
     // *** Axis ***
     // Sticks
