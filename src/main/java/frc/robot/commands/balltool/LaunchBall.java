ee916c482d3e0081003e364336d581ba0ecb4f51
Joshua Turner <joshflyer5tp@gmail.com>
2019-02-14 18:14:07 -0500

    no need to repeat call to extend launcher

--- src/main/java/frc/robot/commands/balltool/LaunchBall.java
+++ src/main/java/frc/robot/commands/balltool/LaunchBall.java
@@ -36,7 +36,6 @@ public class LaunchBall extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    BALL_TOOL.extendLauncher();
   }
 
   // Make this return true when this Command no longer needs to run execute()
